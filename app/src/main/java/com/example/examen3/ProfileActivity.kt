2d3cc0f26b8f5775c30b747b5946a41ccde7597f
Javi-AC91 <anaya.cortes.ricardo.javier@gmail.com>
2026-06-10 21:11:09 -0600

    Diseño de vista de perfil

---
+++ app/src/main/java/com/example/examen3/ProfileActivity.kt
@@ -0,0 +1,17 @@
+package com.example.examen3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.examen3.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
