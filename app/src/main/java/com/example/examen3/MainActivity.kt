0fe54212f5adcbdf6879d0940297bd0dc5a12f36
Javi-AC91 <anaya.cortes.ricardo.javier@gmail.com>
2026-06-10 20:46:05 -0600

    Diseño de vista de registro

--- app/src/main/java/com/example/examen3/MainActivity.kt
+++ app/src/main/java/com/example/examen3/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.examen3
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.examen3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
             insets
+        }
+
+        // Botón iniciar sesión
+        binding.btnLogin.setOnClickListener {
+
+            val password = binding.ttPassword.text.toString()
+
+            if (password == "abc123") {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(
+                    this,
+                    "Contraseña incorrecta",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        // Botón registrarse
+        binding.btnRegister.setOnClickListener {
+
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 }
